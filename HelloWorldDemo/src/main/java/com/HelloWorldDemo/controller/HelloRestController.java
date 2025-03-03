dccf014a535b82cd86e32594d6bac1e740e226e3
Rahuljn2004 <rahul.jaincs21@gmail.com>
2025-03-03 15:44:47 +0530

    resolve errors

--- HelloWorldDemo/src/main/java/com/HelloWorldDemo/controller/HelloRestController.java
+++ HelloWorldDemo/src/main/java/com/HelloWorldDemo/controller/HelloRestController.java
@@ -1,11 +1,12 @@
 package com.HelloWorldDemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Hello from BridgeLabz";
     }
